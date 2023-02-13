e4d064d841678c151af5c1a498becc91c24e1525
DanCesRom <123031318+DanCesRom@users.noreply.github.com>
2023-02-13 13:29:17 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Decoder
+{
+    
+    static string ROT13(string input)
+    {
+        char[] inputArray = input.ToCharArray();
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            char letter = input[i];
+
+            if (letter >= 'A' && letter <= 'Z')
+            {
+                letter = (char)(((letter - 'A' + 13) % 26) + 'A');
+            }
+            inputArray[i] = letter;
+        }
+
+        return new string(inputArray);
+    }
+
+    static void Main(string[] args)
+    {
+        string mensajeCifrado = "GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.";
+        string mensajeDescifrado = ROT13(mensajeCifrado);
+        
+        Console.WriteLine("El mensaje Cifrado era: " + mensajeCifrado);
+        Console.WriteLine("El mensaje Decifrado ahora es " + mensajeDescifrado);
+    }
+
+}
